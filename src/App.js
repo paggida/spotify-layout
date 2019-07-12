2bcc7fa6f4787873d48370fbea897aa11c0a726e
Danilo Paggi <danilopaggi@gmail.com>
2019-07-12 17:10:27 -0300

    Estrutura base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
