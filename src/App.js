6837d38b0d141797c1b98b1a4a38d39416d7cdfd
Danilo Paggi <danilopaggi@gmail.com>
2019-07-15 13:01:21 -0300

    merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react';
 import GlobalStyle from './styles/global';
 import Sidebar from './components/Sidebar';
@@ -8,16 +7,6 @@ const App = () => (
     <GlobalStyle />
     <Sidebar />
   </Fragment>
-=======
-import React from 'react';
-import GlobalStyle from './styles/global';
-
-const App = () => (
-  <div>
-    <GlobalStyle />
-    <h1>Hello world!!</h1>
-  </div>
->>>>>>> fdaaa92e3de33e440a35527d399ac0a55d76ab8f
 );
 
 export default App;
