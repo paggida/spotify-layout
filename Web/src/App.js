0bcc3ae3f39381c54d1d8544b7b2f51f808e4de1
Danilo Paggi <danilopaggi@gmail.com>
2019-07-16 14:32:22 -0300

    W- Configuração do Reactotron

--- Web/src/App.js
+++ Web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; /* Da o acesso aoc componentes filhos as rotas */
 import GlobalStyle from './styles/global';
 import 'rc-slider/assets/index.css';
+import './config/reactotron';
 import { Wrapper, Container, Content } from './styles/components';
 import Routes from './routes';
 import Header from './components/Header';
