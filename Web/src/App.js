33f6234819d96f91a4c6d9e957b6dfca55c3d52e
Danilo Paggi <danilopaggi@gmail.com>
2019-07-16 10:01:59 -0300

    W- Criação estática e estilização da tela Navegar

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; /* Da o acesso aoc componentes filhos as rotas */
 import GlobalStyle from './styles/global';
 import 'rc-slider/assets/index.css';
 import { Wrapper, Container, Content } from './styles/components';
+import Routes from './routes';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 
 const App = () => (
-  <Wrapper>
-    <GlobalStyle />
-    <Container>
-      <Sidebar />
-      <Content>
-        <Header />
-      </Content>
-    </Container>
-    <Player />
-  </Wrapper>
+  <BrowserRouter>
+    <Wrapper>
+      <GlobalStyle />
+      <Container>
+        <Sidebar />
+        <Content>
+          <Header />
+          <Routes />
+        </Content>
+      </Container>
+      <Player />
+    </Wrapper>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ Web/src/pages/Browse/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Container, Title, List, Playlist,
+} from './styles';
+
+const Browse = () => (
+  <Container>
+    <Title>Navegar</Title>
+    <List>
+      <Playlist href="">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/6/64/FFSkinBones.jpg/220px-FFSkinBones.jpg"
+          alt="Playlist"
+        />
+        <strong>Rock dos bons</strong>
+        <p>Codando com as melhores do rock</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://www.somlivre.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/3/9/3903-2.jpg"
+          alt="Playlist"
+        />
+        <strong>Pagocódigo</strong>
+        <p>Esta barra que é codar com você!</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ8PIidm_9V9Tj0HoMi8r1P2OG7bw8e8_k4fYMl5cZJVP2kuvm"
+          alt="Playlist"
+        />
+        <strong>Sapato velho</strong>
+        <p>Codando em cima de sistemas legado</p>
+      </Playlist>
+      <Playlist href="">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/71IjTUGdpNL._SY355_.jpg"
+          alt="Playlist"
+        />
+        <strong>Uma brisa de inverno</strong>
+        <p>Let's codar...</p>
+      </Playlist>
+    </List>
+  </Container>
+);
+
+export default Browse;
