2c285797460b8ffa10e5dc55f0bf0f85ac5b7e6d
paggida <danilopaggi@gmail.com>
2019-07-18 20:17:00 -0300

    W- Ajuste do position na troca de álbum

--- Web/src/store/ducks/player.js
+++ Web/src/store/ducks/player.js
@@ -33,6 +33,7 @@ export default function player(state = INITIAL_STATE, action) {
         currentSong: action.payload.song,
         list: action.payload.list,
         status: Sound.status.PLAYING,
+        position: 0,
       };
     case Types.PLAY:
       return { ...state, status: Sound.status.PLAYING };
