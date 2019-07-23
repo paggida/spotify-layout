528ad7088acbc80641b63905d5c32d813a708df4
Danilo Paggi <danilopaggi@gmail.com>
2019-07-23 15:44:38 -0300

    M- Apresentação dos dados da música atual

--- Mobile/spotify/src/components/Player/index.js
+++ Mobile/spotify/src/components/Player/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   Container,
   CoverBackground,
@@ -10,12 +11,12 @@ import {
   ControlIcon,
 } from './styles';
 
-const Player = () => (
+const Player = ({player, currentEpisode}) => player.current && (
   <Container>
-    <CoverBackground source={{ uri: 'https://s3-sa-east-1.amazonaws.com/gonative/cover3.png' }} />
+    <CoverBackground source={{ uri: currentEpisode.artwork }} />
     <EpisodeInfo>
-      <Title>Título da música</Title>
-      <Author>Autor</Author>
+      <Title>{currentEpisode.title}</Title>
+      <Author>{currentEpisode.artist}</Author>
     </EpisodeInfo>
     <Controls>
       <ControlButton onPress={() => {}}>
@@ -32,3 +33,14 @@ const Player = () => (
 );
 
 export default Player;
+
+const mapStateToProps = state => ({
+  player: state.player,
+  currentEpisode: state.player.podcast
+  ? state.player.podcast.tracks.find(episode=> episode.id === state.player.current)
+  : null,
+
+});
+export default connect(
+  mapStateToProps
+)(Player);
