eeddfe571ae8f83ace198f8add6995b24780c941
Danilo Paggi <danilopaggi@gmail.com>
2019-07-23 15:59:35 -0300

    M- Feature de play e pause

--- Mobile/spotify/src/components/Player/index.js
+++ Mobile/spotify/src/components/Player/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PlayerActions from '~/store/ducks/player';
 import {
   Container,
   CoverBackground,
@@ -11,36 +13,37 @@ import {
   ControlIcon,
 } from './styles';
 
-const Player = ({player, currentEpisode}) => player.current && (
-  <Container>
-    <CoverBackground source={{ uri: currentEpisode.artwork }} />
-    <EpisodeInfo>
-      <Title>{currentEpisode.title}</Title>
-      <Author>{currentEpisode.artist}</Author>
-    </EpisodeInfo>
-    <Controls>
-      <ControlButton onPress={() => {}}>
-        <ControlIcon name="skip-previous" />
-      </ControlButton>
-      <ControlButton onPress={() => {}}>
-        <ControlIcon name="play-circle-filled" />
-      </ControlButton>
-      <ControlButton onPress={() => {}}>
-        <ControlIcon name="skip-next" />
-      </ControlButton>
-    </Controls>
-  </Container>
+const Player = ({
+  player, currentEpisode, play, pause,
+}) => player.current && (
+<Container>
+  <CoverBackground source={{ uri: currentEpisode.artwork }} />
+  <EpisodeInfo>
+    <Title>{currentEpisode.title}</Title>
+    <Author>{currentEpisode.artist}</Author>
+  </EpisodeInfo>
+  <Controls>
+    <ControlButton onPress={() => {}}>
+      <ControlIcon name="skip-previous" />
+    </ControlButton>
+    <ControlButton onPress={player.playing ? pause : play}>
+      <ControlIcon name={player.playing ? 'pause-circle-filled' : 'play-circle-filled'} />
+    </ControlButton>
+    <ControlButton onPress={() => {}}>
+      <ControlIcon name="skip-next" />
+    </ControlButton>
+  </Controls>
+</Container>
 );
-
-export default Player;
 
 const mapStateToProps = state => ({
   player: state.player,
   currentEpisode: state.player.podcast
-  ? state.player.podcast.tracks.find(episode=> episode.id === state.player.current)
-  : null,
-
+    ? state.player.podcast.tracks.find(episode => episode.id === state.player.current)
+    : null,
 });
+const mapDispachToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispachToProps,
 )(Player);
--- Mobile/spotify/src/store/sagas/index.js
+++ Mobile/spotify/src/store/sagas/index.js
@@ -2,12 +2,16 @@ import { all, takeLatest } from 'redux-saga/effects';
 import { PodcastsTypes } from '~/store/ducks/podcasts';
 import { PlayerTypes } from '~/store/ducks/player';
 import { load } from './podcasts';
-import { init, setPodcast } from './player';
+import {
+  init, setPodcast, play, pause,
+} from './player';
 
 export default function* rootSaga() {
   yield all([
     init(),
     takeLatest(PodcastsTypes.LOAD_REQUEST, load),
     takeLatest(PlayerTypes.SET_PODCAST_REQUEST, setPodcast),
+    takeLatest(PlayerTypes.PLAY, play),
+    takeLatest(PlayerTypes.PAUSE, pause),
   ]);
 }
